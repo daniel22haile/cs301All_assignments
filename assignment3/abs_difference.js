c53fdca86e9f60b0445a4dbdc181d4a631707cb4
Daniel Haile <dannymom3@gmail.com>
2020-08-22 21:04:52 -0500

    commit

--- assignment3/abs_difference.js
+++ assignment3/abs_difference.js
@@ -4,8 +4,6 @@ let num2 = prompt("Please enter the second number: ");
 
 if (num1 >= num2) {
     consule.log(Math.abs(num1 - num2));
-}else if (num2 >= num1){
-    consule.log(Math.abs(num2 -num1));
 }else {
-    consule.log("no return");
+    consule.log(Math.abs(num2 -num1));
 }
---
+++ assignment4/power.js
@@ -0,0 +1,8 @@
+let num = 2;
+let count = 0;
+
+do{
+    let result = (Math.pow(num,count));
+    console.log(result + " , \t");
+    count++;
+}while (count <= 31);
