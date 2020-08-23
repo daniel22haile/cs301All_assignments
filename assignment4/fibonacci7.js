b6e79cdced37fef7f96a42bb0935411564e964dc
Daniel Haile <dannymom3@gmail.com>
2020-08-23 13:29:17 -0500

    commit

---
+++ assignment4/fibonacci7.js
@@ -0,0 +1,12 @@
+let num = 25;
+let a = 0;
+let b = 1;
+let fn = a + b;
+document.write(a);
+   
+    do {
+        document.write(" "+fn);
+        fn = a + b;
+        a = b;
+        b = fn;
+    }while (fn < num);
