0f3e7487b67aca10b6dcb06d989cce3332109a26
Daniel Haile <dannymom3@gmail.com>
2020-08-23 16:09:40 -0500

    commit

---
+++ assignment4/phrase_display2.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+let num = prompt("Please enter an integer: ");
+ let user_input_phrase = prompt("Please enter a phrase: ");
+    num = parseInt(num);
+    
+for (  let i = 1; i <= num; i++){
+    console.log(phrase + "\n");
+}
