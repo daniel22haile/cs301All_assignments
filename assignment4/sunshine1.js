6139051e4b86538b72295b332780c8513e470462
Daniel Haile <dannymom3@gmail.com>
2020-08-27 15:56:36 -0500

    commit

--- assignment4/sunshine1.js
+++ assignment4/sunshine1.js
@@ -2,5 +2,5 @@ const prompt = require("prompt-sync")();
 let num = prompt("enter the num ");
     num = parseInt(num);
 for (  let i = 1; i <=num; i++){
-    document.write("happy" + "\n");
+    console.log("happy" + "\n");
 }
