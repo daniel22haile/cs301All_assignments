ab509e5d66c9856f0c8764b9b9efaf84fce31d97
Daniel Haile <dannymom3@gmail.com>
2020-08-27 08:33:18 -0500

    commit

--- assignment4/fibonacci7.js
+++ assignment4/fibonacci7.js
@@ -1,16 +1,3 @@
-//To get the first 25 fibonacci series number
-// let num = 25;
-// let a = 0;
-// let b = 1;
-// let fn = a + b;
-// console.log(a);
-   
-//     do {
-//         console.log(" "+fn);
-//         fn = a + b;
-//         a = b;
-//         b = fn;
-//     }while (fn < num);
 
 let i = 0;
 let j = 1;
