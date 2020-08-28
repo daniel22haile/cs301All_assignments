e9cc4a1d17ca4614dabcc4cc30aa047e12ee9dd8
Daniel Haile <dannymom3@gmail.com>
2020-08-27 21:01:44 -0500

    commit

--- assignment5/check_prime1.js
+++ assignment5/check_prime1.js
@@ -1,15 +1,19 @@
-
+const prompt = require("prompt-sync")();
+let value = prompt("Please enter an integer: ");
+value = parseInt(value);
 
 function isPrime(value) {
-    value = prompt("enter an integer: ");
-    value = parseInt(value);
+
     for(var i = 2; i < value; i++) {
         if(value % i === 0) {
             return false;
+            break;
         }else{
             return true;
+            break;
         }
     }
     return value > 1;
 }
-
+isPrime(value);
+//console.log(value);
