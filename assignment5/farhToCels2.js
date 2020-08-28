460b92bfb991382a172501ab7af2e664705a36dd
Daniel Haile <dannymom3@gmail.com>
2020-08-27 23:18:16 -0500

    commit

---
+++ assignment5/farhToCels2.js
@@ -0,0 +1,13 @@
+
+const prompt = require("prompt-sync")();
+let fahrenheit = prompt("Please enter a temperature in degree fahrenheit: ");
+fahrenheit = parseFloat(fahrenheit);
+
+function farhToCels(fahrenheit){
+    let fTemp = fahrenheit;
+    let fToC = (fTemp - 32) * 5 / 9;
+    let result = fTemp + " degree fahrenheit is " + fToC+ " degree celsius."
+    console.log(result);
+}
+
+farhToCels(fahrenheit);
