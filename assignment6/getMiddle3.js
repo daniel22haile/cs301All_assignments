41556fc88ae931442619bca4e2727fe36b366905
Daniel Haile <dannymom3@gmail.com>
2020-08-29 15:50:14 -0500

    changes

--- assignment6/getMiddle3.js
+++ assignment6/getMiddle3.js
@@ -1,24 +1,10 @@
-
-  let testNumbers = [0, 1 ,2, 3, 4, 5, 6, 7, 8, 9]
-
-
-  function isEven()
-   {
-   let mid = (testNumbers[0] + (testNumbers.length)) / 2;   
-   }
-   
-   function getMiddle(list) 
-   {
-       let mid = (testNumbers[0] + (testNumbers.length)) / 2;
-       if (mid % 2 == 0)
-           {
-           let evenMid = isEven();
-           return evenMid;
-           }
-       else 
-           {
-           return mid;
-           }
-        }
-
-    console.log(getMiddle(testNumbers));
+function getMiddle(arr) {
+    if (arr.length % 2 !== 0) {
+      return arr[Math.floor(arr.length / 2)];
+    } else {
+      return (arr[arr.length / 2] + arr[(arr.length / 2) - 1]) / 2;
+    }
+  }
+  let arr1 = [12,23,45,67,89];
+  let arr2 = [1,2,3,4,5,6,7,8];
+  console.log(getMiddle(arr));
