00ce8e09f828ebf97d80a81b59237a9658060bee
Daniel Haile <dannymom3@gmail.com>
2020-08-29 15:45:37 -0500

    commit

--- assignment6/sumEnds.js
+++ assignment6/sumEnds.js
@@ -1,13 +1,6 @@
 
 function addEnds(list){
-
-    for (let i =0; i < list.length; i++){
-        let total = arr[i] + list([list.length - 1]);
-        
-        return total;
-    }
-    
-}
-let temp = [11,22,33,33];
-console.log(addEnds(temp));
-
+    return list[0]+list[list.length-1];
+       }
+let list = [11,22,33,44,55];
+console.log(addEnds(list));
