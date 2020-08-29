66e2d7e426bfbb3abef810b086fb576fc7797ba6
Daniel Haile <dannymom3@gmail.com>
2020-08-28 22:58:51 -0500

    changes

--- assignment6/sumEnds.js
+++ assignment6/sumEnds.js
@@ -1,22 +1,15 @@
-let list = [12,22,11,32,50];
+let list = [1,2,3,4,5];
 
 function addEnds(list){
-    let sum = 0;
+    //let sum = 0;
     for (let i =0; i < list.length; i++){
         let num1 = list[0];
         let num2 = (list.length - 1);
         let total = num1 + num2;
         return total;
     }
-
-
-
- 
+    
 }
 
-console.log(addEnds(list));
+console.log(addEnds(total));
 
-// let num1 = list(list[0]);
-// let num2 = lists([list.length-1]);
-// let totalArr = num1 + num2;
-// return totalArr;
