ad7524e127fe64dd27e7b240c9d2f45b4c7bbdf9
Daniel Haile <dannymom3@gmail.com>
2020-08-30 22:31:26 -0500

    commit

---
+++ assignment7/prefix2.js
@@ -0,0 +1,16 @@
+function prefix(str1, str2) {
+    let newStr = "";
+    let count = 0;
+    while (true) {
+        if (str1[0 + count] === str2[0 + count]) {
+            newStr = newStr + str1[0 + count];
+            count++;
+        } else {
+            break;
+        }
+
+    }
+    return newStr;
+}
+//expected output //disa
+console.log(prefix("disadvantage", "disallowed"));
---
+++ assignment7/suffix.js
@@ -0,0 +1,15 @@
+function getSuffix(str1, str2) {
+    let endStr = "";
+    let yield = 0;
+    while (true) {
+        if (str1[str1.length - 1 - yield] === str2[str2.length - 1 - yield]) {
+            endStr = str1[str1.length - 1 - yield] + endStr;
+            yield ++;
+        } else {
+            break;
+        }
+    }
+    return endStr;
+}
+//expected output ---- //ddd
+console.log(getSuffix("aaaaddd", "ffffddd"));
